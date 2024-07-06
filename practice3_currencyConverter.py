f281ace5fef41915045e43d217828c44fd51eb71
muk3shh07 <dhamimukesh076@gmail.com>
2024-07-06 12:21:15 +0545

    CurrencyConverter

--- practice3_currencyConverter.py
+++ practice3_currencyConverter.py
@@ -5,6 +5,19 @@ currentDict={}
 for line in lines:
     parsed=line.split('\t')
     currentDict[parsed[0]]=parsed[1]
+
+amount=int(input("Enter the currency amount::"))
+print("Enter the name of currency you want to convert this amount to:")
+[print(item) for item in currentDict.keys()]
+currency = input("Please enter one of the currency you want to convert it to::")
+
+if(currency=='Nepalese Rupee'):
+    print(f"{amount} INR is equal to {amount  / float(currentDict[currency])} {currency}")
+else:
+    print(f"{amount} INR is equal to {amount  * float(currentDict[currency])} {currency}")
+
+
+
     
      
     
